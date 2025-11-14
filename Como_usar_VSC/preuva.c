b05b902539fe0fa0cfeb27b1df042c753fe6a5e0
LuisEduardo02 <72909334+LuisEduardo2002@users.noreply.github.com>
2025-11-14 10:14:57 -0600

    Prueba de compilot

--- Como_usar_VSC/preuva.c
+++ Como_usar_VSC/preuva.c
@@ -1,7 +1,20 @@
-#include<STDIO.H>
+#include <STDIO.H>
 int main()
 {
     /* code */
     printf("HOLA MUNDO2\n");
+    funcion();
     return 0;
 }
+
+void funcion()
+//imprime el nummero 5
+{
+    int n = 5;
+    int i;
+    int fact = 1;
+    for (i = 2; i <= n; ++i) {
+        fact *= i;
+    }
+    printf("%d\n", fact);
+} 
